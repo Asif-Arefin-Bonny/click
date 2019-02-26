af59fec49d8ae5b574ca19a4a9c5009a5c7eb6ab
Asif Arefin Bonny <bonny@techynaf.com>
2019-02-26 18:55:05 +0600

    Range value comes from state

--- src/Components/Click.js
+++ src/Components/Click.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Click extends React.Component {
-    // constructor(props) {
-    //   super(props);
-    //   this.updateRange = this.updateRange.bind(this);
-    // }
-    
-    // updateRange(e) {
-    //   this.props.updateRange(e.target.value);
-    // }
-    
-    render() {
-      // console.log(this.props);
-      const { range } = this.props;
-      return (
-        <div>
-          <input id="range" type="range"
-            value={range}
-            min="60"
-            max="200"
-            step="1"
-            onChange={this.updateRange}
-          />
-          <span id="output">{range}</span>
-        </div>
-      )
-    }
+  // constructor(props) {
+  //   super(props);
+  //   this.updateRange = this.updateRange.bind(this);
+  // }
+
+  // updateRange(e) {
+  //   this.props.updateRange(e.target.value);
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      range: 100
+    };
   }
-  
+
+  render() {
+    //   const { range } = this.props;
+    return (
+      <div>
+        <input
+          id="range"
+          type="range"
+          value={this.state.range}
+          min="60"
+          max="200"
+          step="1"
+          onChange={this.updateRange}
+        />
+        <span id="output">{this.state.range}</span>
+      </div>
+    );
+  }
+}
+
 export default Click;
