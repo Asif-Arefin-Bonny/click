f3ba55cb25b13e5468debc85e0668d90d32de0c1
Asif Arefin Bonny <bonny@techynaf.com>
2019-02-26 20:46:50 +0600

    Experiment

--- src/Components/Click.js
+++ src/Components/Click.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import click1 from "../pulse/click1.wav";
+import click2 from "../pulse/click2.wav";
 
 class Click extends React.Component {
   // constructor(props) {
@@ -11,17 +13,25 @@ class Click extends React.Component {
   // }
   constructor(props) {
     super(props);
+    this.click1 = new Audio(click1);
+    this.click2 = new Audio(click2);
     this.state = {
       range: 0
     };
   }
 
-  updateRange = (e) => {
+  updateRange = e => {
     // this.setState({ range: this.state.range + e.target.value });
     // if (this.state.range >= 200) {
     //     this.setState({ range: 200});
     // }
-    this.setState({range: e.target.value});
+    this.setState({ range: e.target.value });
+  };
+  startStop = () => {
+      for (let i = 0 ; i< this.state.range ; i++){
+           this.click1.play();
+           console.log(i);
+      }
   };
 
   render() {
@@ -38,6 +48,8 @@ class Click extends React.Component {
           onChange={this.updateRange}
         />
         <span id="output">{this.state.range}</span>
+        <button onClick={this.startStop} >Start</button>
+        {/* <button onClick={this.startStop}>{playing ? "Stop" : "Start"}</button> */}
       </div>
     );
   }
